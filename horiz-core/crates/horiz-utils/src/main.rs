076477877d9fcaf7fee6d6036d5499aad61fcef2
malken21 <77374813+malken21@users.noreply.github.com>
2026-04-21 20:22:18 +0900

    Update main.rs

--- horiz-core/crates/horiz-utils/src/main.rs
+++ horiz-core/crates/horiz-utils/src/main.rs
@@ -1,11 +1,12 @@
 use horiz_utils;
 use std::env;
+use std::path::Path;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let cmd = env::current_exe()
-        .ok()
-        .and_then(|p| p.file_name().map(|n| n.to_string_lossy().into_owned()))
+    let cmd = args.get(0)
+        .and_then(|path| Path::new(path).file_name())
+        .map(|name| name.to_string_lossy().into_owned())
         .unwrap_or_default();
 
     match cmd.as_str() {
